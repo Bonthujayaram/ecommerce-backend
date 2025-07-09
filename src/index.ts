0d820579bf14a1163a2d44feec81f87f5a4c0d0a
Bonthu Jayaram <131760182+Bonthujayaram@users.noreply.github.com>
2025-07-09 15:00:03 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5002;
 app.use(cors({
   origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 
           'http://localhost:5176', 'http://localhost:5177', 'http://localhost:5178', 
-          'http://localhost:5179', 'http://localhost:5180'],
+          'http://localhost:5179', 'http://localhost:5180','https://eco-ai-asst.netlify.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
